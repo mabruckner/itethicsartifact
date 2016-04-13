c4a2f1510d3834f8e8532266d717206a4f0b7b03
Mitchell Bruckner <mbruckner807@gmail.com>
2016-04-12 22:16:20 -0500

    integrate json data

--- js/main.js
+++ js/main.js
@@ -2,11 +2,20 @@ var app = angular.module('choose', []).config([
         function() {
         }]);
 
-app.controller('WizardController',['$scope',
-        function($scope) {
-           $scope.currentquestion = 0;
-           $scope.questions = ['asdfhlkjdsa hjlksa fdsaj', 'asdlhfiuvlj ij hjidk lsj'];
-           $scope.answers = $scope.questions.forEach(function(trash) {
-               return true;
-           },this);
+app.controller('WizardController',['$scope','$http',
+        function($scope,$http) {
+            $scope.licenses = [];
+            $scope.questions = ['asdfhlkjdsa hjlksa fdsaj', 'asdlhfiuvlj ij hjidk lsj'];
+            $http.get('/license_choices.json').then(function(dat){
+                console.log(dat);
+                $scope.licenses = dat.data.licenses;
+                $scope.questions = dat.data.questions;
+            },function(err) {
+                console.log("ERR");
+                console.log(err);
+            });
+            $scope.currentquestion = 0;
+            $scope.answers = $scope.questions.forEach(function(trash) {
+                return true;
+            },this);
         }]);
